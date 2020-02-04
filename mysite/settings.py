164a734582463681bb7fd75e07062e1fc8790139
Shantanu Minhas <shantanuminhas@gmail.com>
2020-02-04 13:24:24 -0600

    Updated Settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_DJANGO')
+DEBUG = True
 
 ALLOWED_HOSTS = ['shansfirstwebsite.herokuapp.com', '127.0.0.1']
 
