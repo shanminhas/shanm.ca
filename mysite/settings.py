40ad4f74dcfa3a9c975ce83dbb2fcf89e0aae785
Shantanu Minhas <shantanuminhas@gmail.com>
2020-02-10 17:32:40 -0600

    Fixed Polls Error Again and Changed Polls Background

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,6 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DJANGO_DEBUG') == True)
+#DEBUG = True
 
 ALLOWED_HOSTS = ['shansfirstwebsite.herokuapp.com', '127.0.0.1']
 
